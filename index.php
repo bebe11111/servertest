ed4a82828d196174eeebf7edfc69a8d9ea0e12c1
Bor Bence <132979209+bebe11111@users.noreply.github.com>
2026-03-25 22:56:04 +0100

    test_v9 (working?)

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 echo "<h1>Railway PHP + NGINX működik!</h1>";
 phpinfo();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>És tényleg fut</p>
+</body>
+</html>
